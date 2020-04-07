523808b3ccd95a2fff47d56f72528e6058c98282
pouriap19 <pouriap19@yahoo.com>
2020-04-07 18:15:37 +0430

    fixed bug: some requests were never removed
    now we remove requests from allRequests in onCompleted() callback which is run for all requests
    did some clean up and commenting

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
+//TODO: check all API levels and see exactly what is the minimum version
 
-//all requests made by Firefox are stored here temporarily until we get their response
+// all requests made by Firefox are stored here temporarily until we get their response
 var allRequests = new FixedSizeMap(100);
 
 // the last 20 downloadable items are stored here with their informations such as cookies,time,etc.
+//todo: options
 var allDlItems = new FixedSizeMap(20);
 
 // utility function
 var Utils = new Utils();
 
-
-//TODO: check all API levels and see exactly what is the minimum version
 
 browser.webRequest.onBeforeSendHeaders.addListener(
 	doOnBeforeSendHeaders, {
@@ -25,31 +25,46 @@ browser.webRequest.onHeadersReceived.addListener(
 	["responseHeaders"]
 );
 
+browser.webRequest.onCompleted.addListener(
+	doOnCompleted, {
+		urls: ["*://*/*"]
+	},
+	["responseHeaders"]
+);
 
-// is used to store cookies, referer and other request headers that are unavailable in reponse
+
+/**
+ * Runs before a request is sent
+ * Is used to store cookies, referer and other request info that is unavailable in reponse
+ */
 function doOnBeforeSendHeaders(details){
 
-	console.log(details.requestHeaders);
-
 	let requestId = details.requestId;
-	let url = details.url;
 	let cookieHeader = Utils.getHeader(details.requestHeaders, "cookie");
 	let cookies = (cookieHeader === undefined)? "" : cookieHeader.value;
 	let refererHeader = Utils.getHeader(details.requestHeaders, "referer");
 	let referer = (refererHeader === undefined)? "" : refererHeader;
-	let time = Date.now();
-	let dlItem = {
-		url: url,
-		cookies: cookies,
-		referer: referer,
-		time: time
-	};
+	let origin = details.originUrl;
+	let time = details.timeStamp;
+
+	//create a dlItem object and put necessary information in it
+	let dlItem = {};
+	dlItem.cookies = cookies;
+	dlItem.referer = referer;
+	dlItem.origin = origin;
+	dlItem.time = time;
+
+	//put the dlItem in allRequests so it can be accessed by it's requestId when response is received
+	allRequests.put(requestId, dlItem);
 
 	console.log("sending: ", requestId);
-
-	allRequests.put(requestId, dlItem);
 }
 
+/**
+ * Runs once response headers are received 
+ * We create a dlItem here if the response matches our criteria and add it 
+ * to our list of downloads
+ */
 function doOnHeadersReceived(details) {
 	
 	console.log("receiving: ", details.requestId);
@@ -62,28 +77,26 @@ function doOnHeadersReceived(details) {
 		return;
 	}
 
-	let cookies = requestOfThisResponse.cookies;
-	let referer = requestOfThisResponse.referer;
-	let time = requestOfThisResponse.time;
-
-	//remove asap to save memory
-	allRequests.remove(requestId);
-
-	//creating a new object because we deleted the original from allRequests
+	//creating a new dlItem object because will delete the original from allRequests
+	//in doOnCompleted() after the request is completed
 	let dlItem = {};
 	dlItem.requestId = requestId;
 	dlItem.url = url;
-	dlItem.cookies = cookies;
-	dlItem.referer = referer;
-	dlItem.time = time;
+	dlItem.cookies = requestOfThisResponse.cookies;
+	dlItem.referer = requestOfThisResponse.referer;
+	dlItem.origin = requestOfThisResponse.origin;
+	dlItem.time = requestOfThisResponse.time;
 
 	if (url.startsWith("ws://") || url.startsWith("wss://")) {
 		return {};
 	}
 
+	// first we look at content-length
+	// if the file is big enough we consider it a download
 	let contentLengthHeader = Utils.getHeader(details.responseHeaders, "content-length");
 	if (typeof contentLengthHeader !== 'undefined') {
 		var fileSizeMB = contentLengthHeader.value / 1000000;
+		//todo: options
 		if (fileSizeMB > 5) {
 			dlItem.debug_reason = "content length: " + fileSizeMB.toFixed(1) + "MB";
 			addToAllDlItems(dlItem);
@@ -91,6 +104,8 @@ function doOnHeadersReceived(details) {
 		return {};
 	}
 
+	// if content-lenght is not specified we look at content-type
+	// if content-type is 'application/octet-stream' we consider it a download
 	let contentTypeHeader = Utils.getHeader(details.responseHeaders, "content-type");
 	if (typeof contentTypeHeader !== 'undefined') {
 		var contentType = contentTypeHeader.value;
@@ -101,6 +116,9 @@ function doOnHeadersReceived(details) {
 		return {};
 	}
 
+	// if contnet lenght and type are unavailable we look at the file extension
+	// we consider the file a download based on our inclusion/exclusion list
+	//todo: options
 	let extensionsToNotDownload = ['jpg', 'jpeg', 'png', 'gif', 'css', 'js', 'html', 'htm', 'php', 'asp', 'jsp'];
 	let extension = Utils.getExtensionFromURL(url);
 	if (extension && !extensionsToNotDownload.includes(extension)) {
@@ -109,19 +127,30 @@ function doOnHeadersReceived(details) {
 		return {};
 	}
 
+	// if the response doesn't math any of our criteria then it is not a download and we ignore it
 	console.log("ignoring url: ", url);
 	return {};
+
+	function addToAllDlItems(dlItem){
+		//we do this here because we don't want to run hash on requests we will not use
+		dlItem.hash = md5(dlItem.url);
+		//we put hash of URL as key to prevent the same URL being added by different requests
+		allDlItems.put(dlItem.hash, dlItem);
+	}
+
 }
 
-function addToAllDlItems(dlItem){
-	//we do this here because we don't want to hash the requests we will not add to dl list
-	dlItem.hash = md5(dlItem.url);
-	//we put hash of URL as key to prevent the same URL being added by different requests
-	allDlItems.put(dlItem.hash, dlItem);
+/**
+ * Runs once a request is completed
+ */
+function doOnCompleted(details){
+	//remove the original dlItem from allRequests to save memory
+	//this isn't really necessary because allRequest is a fixed sized map
+	allRequests.remove(details.requestId);
 }
 
 
-// Fixed size array class
+// Fixed size map class
 function FixedSizeMap(size) {
 
 	size = (Number(size));
@@ -173,7 +202,7 @@ function DownloadsList(size) {
 	}
 }
 
-// A Util class of course
+// A utility class of course!
 function Utils(){
 
 	this.getHeader = function (headersArray, headerName){
@@ -228,8 +257,7 @@ function Utils(){
 		port.disconnect();
 
 	}
-	
-	
+		
 	this.downloadWithBrowser = function (url) {
 		browser.downloads.download({
 			saveAs: true,
