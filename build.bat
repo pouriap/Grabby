2001ea03f2aa216514a00577faf1622be376a90f
pouriap19 <pouriap19@yahoo.com>
2020-04-29 20:47:25 +0430

    removed '_extras' from history

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 set name=addon.xpi
 rm %name%
-zip -r %name% . -x "*.xpi" "*.bat" "*.sh" "*.psd" "*.md" "*.code-workspace" "*.gitignore" ".git/" ".git/*" "extras/" "extras/*"
+zip -r %name% . -x "*.xpi" "*.bat" "*.sh" "*.psd" "*.md" "*.code-workspace" "*.gitignore" ".git/" ".git/*" "_extras/" "_extras/*"
