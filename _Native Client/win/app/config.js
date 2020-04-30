c685c81727dc95a8d820b7debf9966688974fe15
pouriap19 <pouriap19@yahoo.com>
2020-04-30 20:39:05 +0430

    modified native client code for Download Grab

--- _Native Client/win/app/config.js
+++ _Native Client/win/app/config.js
@@ -1,36 +1,4 @@
 'use strict';
 
-exports.version = '0.1.0';
-exports.id = 'com.mybrowseraddon.node';
-
-exports.ids = {
-  "chrome": [
-    "dkhhkjpckbmkfdicijemmdaieaagkcco",  /* chrome: open-in-ie */
-    "dcfemfgnhfagfkeglchakeobdjdgcanl",  /* chrome: open-with-idm */
-    "nhpfgmjcpbdcbmhjbhogflbbmdbomfmb",  /* chrome: open-in-vlc */
-    "ehndikigeekodlnbohjndjjiikphickb",  /* chrome: open-with-pdf */
-    "epgilldoipfbdlneomlmegekaoonchcj",  /* chrome: open-with-idm */
-    "mgnjlfljmmnpkliogobdekjohjgfimpc",  /* chrome: pdf-tools */
-    "gaffigalddbnemnpanbjhfcfopkaagoi",  /* chrome: media-tools */
-    "beklnljhbalhpnbkllnbladeopnhnnfc",  /* chrome: multi-threaded-dm */
-    "enijlgojimhgkefecingdinhcfpinbhp",  /* opera: open-in-ie */
-    "anlkimmcefcceiddhggegodhkgkndhic",  /* opera: open-in-chrome */
-    "feinkgpipgaoonanlbgcpbkbjaggdaml",  /* opera: open-with-idm */
-    "bfannejkbkejigonaemcmbbkpnfadlii",  /* opera: open-in-vlc */
-    "iabonojeikmegigkkgjmdicdciollhfk",  /* opera: open-with-pdf */
-    "gepdmknboomlgooaehkkjlhehpobjcko",  /* opera: pdf-tools */
-    "egiidbehgmjiibofjmhhbcdeagjcjdci",  /* opera: media-tools */
-    "hfdagjdkeglbmefgmeahkpbkhoiichnk",  /* opera: multi-threaded-dm */
-  ],
-  "firefox": [
-    "jid1-q2yAXbZkz2GZH4@jetpack",             /* firefox: open-in-ie */
-    "jid1-r2yWXsZku2AJH2@jetpack",             /* firefox: open-in-chrome */
-    "{6c0194b2-e924-4987-b9a9-6836a5e46752}",  /* firefox: open-with-idm */
-    "{e427e22d-70e7-4132-8764-8bab93601197}",  /* firefox: open-in-vlc */
-    "{2582ab30-4fca-475f-88d0-c1a9b9ed978f}",  /* firefox: open-with-pdf */
-    "{9cd72ecf-3268-4966-a6e3-d82b2eeef4ad}",  /* firefox: open-with-idm */
-    "{7044c7d3-e541-4efb-8c7e-95a6a9d10a73}",  /* firefox: pdf-tools */
-    "{62a66cf9-5871-48cc-abc2-13f9d3787e8e}",  /* firefox: media-tools */
-    "{815c985b-2a7c-4f59-9106-6a6c07055172}",  /* firefox: multi-threaded-dm */
-  ]
-};
+exports.version = '0.1';
+exports.id = 'download.grab.pouriap';
--- _Native Client/win/app/host.js
+++ _Native Client/win/app/host.js
@@ -1,67 +1,52 @@
 'use strict';
 
-var config = require('./config.js');
+var {execFileSync} = require('child_process');
+var {readFileSync} = require('fs');
 
 // closing node when parent process is killed
 process.stdin.resume();
 process.stdin.on('end', () => process.exit());
 
-function observe (request, push, done) {
-  let close;
-  const exception = e => {
-    push({
-      type: 'exception',
-      error: e.message
-    });
-    close();
-  };
-  close = () => {
-    process.removeListener('uncaughtException', exception);
-    done();
-    close = () => {};
-  };
-  process.addListener('uncaughtException', exception);
+function observe(message, push, done) {
+	let close;
+	const exception = e => {
+		push({
+			type: 'exception',
+			error: e.message
+		});
+		close();
+	};
+	close = () => {
+		process.removeListener('uncaughtException', exception);
+		done();
+		close = () => {};
+	};
+	process.addListener('uncaughtException', exception);
 
-  if (request.method === 'spec') {
-    push({
-      version: config.version,
-      env: process.env,
-      release: process.release,
-      platform: process.platform,
-      arch: process.arch,
-      versions: process.versions,
-      separator: require('path').sep,
-      tmpdir: require('os').tmpdir()
-    });
-    close();
-  }
-  else if ('script' in request) {
-    const vm = require('vm');
-    const sandbox = {
-      version: config.version,
-      env: process.env,
-      push,
-      close,
-      args: request.args,
-      // only allow internal modules that extension already requested permission for
-      require: (name) => (request.permissions || []).indexOf(name) === -1 ? null : require(name)
-    };
-    const script = new vm.Script(request.script);
-    const context = new vm.createContext(sandbox);
-    script.runInContext(context);
-  }
-  else {
-    push({
-      type: 'context',
-      error: 'cannot find "script" key in your request. Closing connection...'
-    });
-    close();
-  }
+	if (message.type === 'get_available_dms') {
+		//todo: sanitize user input if possible
+		let availableDMs = [];
+		execFileSync("FlashGot.exe", ['-o', 'availableDMs']);
+		let file = readFileSync('availableDMs', {encoding: 'utf8', flag: 'r'});
+		let lines = file.split('\n');
+		for(let line of lines){
+			if(line.indexOf('|OK') !== -1){
+				let DMName = line.split("|")[0].trim();
+				availableDMs.push(DMName);
+			}
+		}
+		let message = {type: 'available_dms', availableDMs: availableDMs};
+		push(message);
+		close();
+	} else {
+		push({type: 'unsupported'});
+		close();
+	}
 }
 /* message passing */
 var nativeMessage = require('./messaging');
 process.stdin
-  .pipe(new nativeMessage.Input())
-  .pipe(new nativeMessage.Transform(observe))
-  .pipe(new nativeMessage.Output())
-  .pipe(process.stdout);
+	.pipe(new nativeMessage.Input())
+	.pipe(new nativeMessage.Transform(observe))
+	.pipe(new nativeMessage.Output())
+	.pipe(process.stdout);
