a92b72952a6b2d104d8a358924d3f9d63ea5e2c7
pouria <pouriap19@yahoo.com>
2022-08-19 12:37:00 +0430

    fixed issue with gulp not compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp'); 
 var ts = require('gulp-typescript');
-var tsProject = ts.createProject("tsconfig.json");
+var tsProject = ts.createProject("./tsconfig.json");
 var less = require('gulp-less');
 
 gulp.task('default', function(){
-	gulp.src('*/src/icons/**').pipe(gulp.dest('dist'));
-	gulp.src('*/src/libs/**').pipe(gulp.dest('dist'));
+	gulp.src('./src/icons/**/*', { "base" : "./src" }).pipe(gulp.dest('dist'));
+	gulp.src('./src/libs/**/*', { "base" : "./src" }).pipe(gulp.dest('dist'));
     gulp.src('./src/**/*.jpg').pipe(gulp.dest('dist'));
 	gulp.src('./src/**/*.png').pipe(gulp.dest('dist'));
 	gulp.src('./src/**/*.gif').pipe(gulp.dest('dist'));
 	gulp.src('./src/**/*.html').pipe(gulp.dest('dist'));
     gulp.src('./src/**/*.less').pipe(less()).pipe(gulp.dest('dist'));    //do all less files
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));    //do the typescript files
+    return tsProject.src().pipe(tsProject())
+	.on("error", () => { /* Ignore compiler errors */})
+	.js.pipe(gulp.dest("dist"));    //do the typescript files
 });
