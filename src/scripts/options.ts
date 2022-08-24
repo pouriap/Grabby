64fe2b00a93cf77a320579292a4f83c9568b914a
pouria <pouriap19@yahoo.com>
2022-08-24 19:56:57 +0430

    wip

--- src/scripts/options.ts
+++ src/scripts/options.ts
@@ -1,23 +1,147 @@
-type OPTIONS =
+namespace Options
 {
-	[key: string]: any;
-	overrideDlDialog: any;
-	playMediaInBrowser: any;
-	dlListSize: any;
-	showOnlyTabDls: any;
-	grabFilesLargerThanMB: any;
-	excludeWebFiles: any;
-	includedExts: any;
-	includedMimes: any;
-	excludedExts: any;
-	excludedMimes: any;
-	forcedExts: any;
-	forcedMimes: any;
-	blacklistDomains: any;
-	defaultDM: any;
+	class Options
+	{
+		[index: string]: unknown;
+		overrideDlDialog: boolean = true;
+		playMediaInBrowser: boolean = true;
+		dlListSize: number = 20;
+		showOnlyTabDls: boolean = true;
+		grabFilesLargerThanMB: number = 5;
+		excludeWebFiles: boolean = true;
+		includedExts: string[] = [];
+		includedMimes: string[] = [];
+		excludedExts: string[] = [];
+		excludedMimes: string[] = [];
+		forcedExts: string[] = [];
+		forcedMimes: string[] = [];
+		blacklistDomains: string[] = [];
+		defaultDM: string = '';
+	}
+
+	type OptionNames<T> = 
+	{
+		[Property in keyof Options]: T;
+	}
+
+	export function load()
+	{
+		let defaults = new Options();
+		for(let optionName in defaults)
+		{
+			defaults[optionName] = data[optionName].default;
+		}
+		
+		let loadedOpts = browser.storage.local.get(defaults);
+
+		//put the normal options
+		for(let optionName of optionNames)
+		{
+			Options[optionName] = loadedOpts[optionName];
+		}
+
+		//put the options that need processing
+
+	}
+
+	type OptionData<T> = 
+	{
+		type: string;
+		default: T;
+		desc: string;
+		endsection?: boolean;
+		attrs?: pair[];
+		getListData?: (arg?: any) => string[];
+	}
+
+	let data: OptionNames<OptionData<unknown>> = 
+	{
+		overrideDlDialog: <OptionData<boolean>> {
+			type: 'checkbox',
+			default: true,
+			desc: "Override Firefox's download dialog",
+		},
+		playMediaInBrowser: <OptionData<boolean>> {
+			type: 'checkbox',
+			default: true,
+			desc: "Do not offer to download files that can be displayed inside browser (text, media and pdf)",
+		},
+		dlListSize: <OptionData<number>> {
+			type: 'textbox',
+			default: 20,
+			desc: 'Number of items to keep in downloads history:',
+		},
+		showOnlyTabDls: <OptionData<boolean>> {
+			type: 'checkbox',
+			default: true,
+			desc: 'Show only downlods originated from current tab in the popup list',
+			endsection: true,
+		},
+	
+		grabFilesLargerThanMB: <OptionData<number>> {
+			type: 'textbox',
+			default: 0,
+			desc: "Ignore files smaller than (MB):"
+		},
+		excludeWebFiles: <OptionData<boolean>> {
+			type: 'checkbox',
+			default: true,
+			desc: "Ignore common web files (images, fonts, etc.)",
+		},
+		excludedExts: <OptionData<string>> {
+			type: 'textbox',
+			default: '',
+			desc: "Ignore files with these extensions:",
+			attrs: [{name: 'placeholder', value: 'ext1,ext2,ext3,...'}],
+		},
+		includedExts: <OptionData<string>> {
+			type: 'textbox',
+			default: '',
+			desc: "Detect files with these extensions as downloads:",
+			attrs: [{name: 'placeholder', value: 'ext1,ext2,ext3,...'}],
+		},
+		forcedExts: <OptionData<string>> {
+			type: 'textbox',
+			default: '',
+			desc: "Directly download files with these extensions with my default manager:",
+			attrs: [{name: 'placeholder', value: 'ext1,ext2,ext3,...'}],
+		},
+		blacklistDomains: <OptionData<string>> {
+			type: 'textbox',
+			default: '',
+			desc: "Do not grab from these domains:",
+			attrs: [{name: 'placeholder', value: 'example.com,example.org,...'}],
+			endsection: true,
+		},
+	
+		defaultDM: <OptionData<string>> {
+			type: 'dropdown',
+			default: '',
+			desc: "Default download manager: ",
+			getListData: function(dlgInstance: DownloadGrab){
+				return dlgInstance.availableDMs;
+			},
+		},
+
+		excludedMimes: <OptionData<string>> {
+			type: 'processed',
+			default: '',
+			desc: '',
+		},
+		includedMimes: <OptionData<string>> {
+			type: 'processed',
+			default: '',
+			desc: '',
+		},
+		forcedMimes: <OptionData<string>> {
+			type: 'processed',
+			default: '',
+			desc: '',
+		},
+	}
 }
 
-namespace Options
+namespace OptionsSSS
 {
 	/**
 	 * Loads options from browser storage
